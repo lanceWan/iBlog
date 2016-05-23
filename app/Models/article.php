d1f55a9e62ce2b7d25a20bdd0cf7a477752dabfe
wanli <709344897@qq.com>
2016-05-23 10:53:52 +0800

    忽略不必要文件

--- app/Models/article.php
+++ app/Models/article.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\ActionAttributeTrait;
-class Article extends Model
+class article extends Model
 {
 	use ActionAttributeTrait;
 	
